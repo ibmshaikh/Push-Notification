683b8e89da81ea790781c8c83c1f96d26c3b8c11
Ibrahim <shaikhibrahim7737@gmail.com>
2018-05-02 16:39:34 +0530

    Push Notification File

---
+++ notification.py
@@ -0,0 +1,20 @@
+from pyfcm import FCMNotification
+
+push_service = FCMNotification(api_key="AAAAaccXLck:APA91bHEthjjmJJ0vp8te617ztOkMNGVwTLBhzHAtihQ3NwTCMC1YlhmlzpC2T4VziFm7KgqhAOT0opcxQmKKgM_pSEjTfvRfIQvWQUAZHOo0OrBUoaDC13yEkc3BUWw7CT7hg2O5_8B")
+
+# OR initialize with proxies
+
+# Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
+
+registration_id = "c4yFKTrfG9s:APA91bGdFDzVm_VPtgXDETEdUzGqIVeKPTrR2fOy8rY-4SxbYGWeVgxEss86LFjA-w43O4uuOtfCm4J6pxDaNdIV7QOh1S548Y5V0_jd7AqtMWo52ceaqthJHVmbcI-AkEu1476PTi6M"
+message_title = "SNC"
+message_body = "Notification Send By Python"
+result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+
+# Send to multiple devices by passing a list of ids.
+#  registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
+#message_title = "Uber update"
+#message_body = "Hope you're having fun this weekend, don't forget to check today's news"
+#result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+
+print result
